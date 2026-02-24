4bf7fa6a7a4ca7ad7218ccc17a1db28adfbc9bf7
rohnshrma <rohn11shrma@gmail.com>
2026-02-24 19:16:12 +0530

    feat: Add user and cart routes to the server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import { config } from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import morgan from "morgan";
 config();
 connectDB();
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
 app.use(morgan("dev"));
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server started on port : ", PORT));
